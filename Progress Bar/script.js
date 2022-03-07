9c220a9e3ef08d5347b4601fbe6410d8ceb19e54
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2022-03-07 18:54:59 +0530

    Progress Bar files edited.

--- Progress Bar/script.js
+++ Progress Bar/script.js
@@ -1,6 +1,7 @@
 const progressBar = document.querySelector("#progress-bar");
 const section = document.querySelector("section");
 
+/** Function to animate progress bar */
 const animateProgressBar = () => {
     let scrollDistance = -section.getBoundingClientRect().top;
     let progressBarWidth = (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
